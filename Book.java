768bfe203e09cfeda5f996b3d9e16a4c247832b8
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2019-08-30 15:10:18 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,55 @@
+import java.util.*;
+class BookDemo
+{
+	String title,author;
+	double price;
+	int pages;
+	BookDemo()
+	{
+		title="\0";
+		author="\0";
+		price=0.0;
+		pages=0;
+		
+		
+	}
+	void set()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter title of the book,author name,price,no of pages=");
+		title=sc.next();
+		author=sc.next();	
+		price=sc.nextInt();
+		pages=sc.nextInt();
+	}
+
+	public String toString()
+	{
+		return "tile="+title+",author="+author+",price="+price+"pages="+pages;
+		
+	}
+	void get()
+	{
+	System.out.println(toString());
+	}
+}
+class Book
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		System.out.println("enter number of objects to be created=");
+		n=sc.nextInt();
+		BookDemo a[]=new BookDemo[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=new BookDemo();
+		}
+		for(int i=0;i<n;i++)
+		{
+			a[i].set();
+			a[i].get();
+		}
+	}
+}
