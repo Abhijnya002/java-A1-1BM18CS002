c98c661b605d9d025e92ebccb35eadcadb850627
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2019-09-07 21:51:36 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Prime
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the integer");
+		int n=sc.nextInt();
+		int i;
+		for(i=0;i<n;i++)
+		{
+			if(isprime(i)==1)
+			{
+				if(isprime(n-i)==1)
+				{
+					System.out.println("yes");
+				}
+				else
+				{
+					System.out.println("no");
+				}	
+			}
+			else
+			{
+				System.out.println("no");
+			}
+		}
+	}
+             static int isprime(int x){
+		int j,prime=1;
+		for(j=2;j<=(x/2);j++)
+		{
+			if(x%j==0)
+			{
+				return 0;
+			}
+			else
+			{
+				return 1;
+			}
+		}
+	}
+}
