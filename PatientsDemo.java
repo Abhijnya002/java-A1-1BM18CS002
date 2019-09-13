b4ff9b25fedaf2b656705c73ccc46030becaedb9
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2019-09-13 23:16:01 +0530

    Add files via upload

---
+++ PatientsDemo.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Patients
+{
+	String id,name,doc;
+	void input()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the id, name and doctor");
+		id=sc.next();
+		name=sc.next();
+		doc=sc.next();
+	}
+	void display()
+	{
+		System.out.println("name="+name);
+		System.out.println("id="+id);
+		System.out.println("doctor="+doc);
+		
+	}
+}
+class PatientsDemo
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of patients");
+		int n=sc.nextInt();
+		Patients ob[]=new Patients[n];
+		int i,j;
+		for(i=0;i<n;i++)
+		{
+			ob[i]=new Patients();
+			ob[i].input();
+			ob[i].display();
+		}
+		System.out.println("Enter the doctors name ");
+		String doctor=sc.next();
+		for(j=0;j<n;j++)
+		{
+			System.out.println("The patients are");
+			if(doctor.equals(ob[j].doc))
+			{
+				System.out.println(ob[j].name);	
+			}
+
+		}
+	}
+}
+		
