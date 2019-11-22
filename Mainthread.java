cf6ef580c1f3a74ea5ca0df1d164505803b54821
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2019-11-22 14:42:05 +0530

    Add files via upload

---
+++ Mainthread.java
@@ -0,0 +1,57 @@
+class T1 implements Runnable
+{
+	Thread t;
+	T1()
+	{
+		t=new Thread(this,"T1");
+		t.start();
+	}
+	public void run()
+	{
+		while(true)
+		{
+			System.out.println("BMSCE");
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println("interrupted");
+			}
+		}
+	}
+}
+class T2 implements Runnable
+{
+	Thread t;
+	T2()
+	{
+		t=new Thread(this,"T2");
+		t.start();
+	}
+	public void run()
+	{
+		while(true)
+		{
+			System.out.println("CSE");
+			try
+			{
+				Thread.sleep(2000);
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println("interrupted");
+			}
+		}
+	}
+}
+class Mainthread
+{
+	public static void main(String args[])
+	{
+		new T1();
+		new T2();
+	}
+}
+
